85b8e68305de3b8054ab0a82dcf912e4e5a151ec
Vlad Paraschiv <vlad.paraschiv@globallogic.com>
2024-05-05 09:55:31 +0300

    Added movie rating mechanisms and watched movies functionality

--- src/index.js
+++ src/index.js
@@ -1,29 +1,33 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import StarRating from './StarRating';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import StarRating from './StarRating'
 
 function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+    const [movieRating, setMovieRating] = useState(0)
 
-  return (
-    <div>
-      <StarRating maxRating={10} color='blue' onSetRating = {setMovieRating} />
+    return (
+        <div>
+            <StarRating
+                color="blue"
+                maxRating={10}
+                onSetRating={setMovieRating}
+            />
 
-      <p>These movies have a rating of {movieRating}</p>
-    </div>
-  );
+            <p>These movies have a rating of {movieRating}</p>
+        </div>
+    )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} messages={["Terrible", "Bad",
+    <React.StrictMode>
+        <App />
+        {/* <StarRating maxRating={5} messages={["Terrible", "Bad",
      "Okay","Good","Amazing"]}/>
     <StarRating size={24} color='red' className="test" defaultRating={3}/>
 
-    <Test></Test>
-  </React.StrictMode>
-);
+    <Test></Test> */}
+    </React.StrictMode>
+)
